9185fc432164f74a860d0a3478753ef2d85c5e4c
Honeyaps <honeyaps12345@gmail.com>
2024-06-07 16:32:24 +0530

    first

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,52 @@
-import Navbar from "../components/nav";
-import "./page.css";
+import React, { useState } from 'react';
+import Navbar from '../components/nav';
+import './page.css';
+import { FiInstagram } from 'react-icons/fi';
+import axios from 'axios';
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.name = formData.name ? '' : 'Name is required';
+    tempErrors.email = formData.email ? '' : 'Email is required';
+    tempErrors.email = /\S+@\S+\.\S+/.test(formData.email) ? '' : 'Email is not valid';
+    tempErrors.message = formData.message ? '' : 'Message is required';
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === '');
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validate()) {
+      try {
+        const response = await axios.post('http://localhost:4400/message', formData);
+        alert("Message sent succesfuly");
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        });
+      } catch (error) {
+        alert('Error submitting form');
+      }
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -28,7 +73,7 @@ export default function Home() {
       {/* ABOUT SECTION */}
       <div className="about_sec">
         <div className="img_div">
-          <img src="IMG_20221210_234743.jpg" className="img"></img>
+          <img src="IMG_20221210_234743.jpg" className="img" alt="Hemant Singh" />
         </div>
         <div className="about">
           <h1 className="h1_abt">About Me</h1>
@@ -60,40 +105,180 @@ export default function Home() {
         </div>
       </div>
 
-      <br/><br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
 
       {/* LANGUAGES */}
-     
+
       <h1 className="wik">What I Know?</h1>
       <div className="lang_div_contnr">
-        
         <div className="frontend">
           <h2 className="lang_head">Frontend</h2>
-          <br/>
+          <br />
           <span>HTML</span>
-          <span>CSS</span> <br/><br/>
+          <span>CSS</span> <br />
+          <br />
           <span>Tailwind</span>
-          <span>Javascript</span> <br/><br/>
+          <span>Javascript</span> <br />
+          <br />
           <span>React js</span>
         </div>
 
         <div className="backend">
           <h2 className="lang_head">Backend</h2>
-          <br/>
+          <br />
           <span>Node.js</span>
-          <span>Express</span> <br/><br/>
+          <span>Express</span> <br />
+          <br />
         </div>
 
         <div className="database">
           <h2 className="lang_head">Database</h2>
-          <br/>
+          <br />
           <span>MongoDB</span>
-          <span>Firebase</span><br/><br/>
+          <span>Firebase</span>
+          <br />
+          <br />
           <span>MongoDB Atlas</span>
+        </div>
+
+        <div className="projects">
+          <h2 className="lang_head">Projects</h2>
+          <br />
+          <div className="inner_proj_div">
+            <a href="https://github.com/Honeyaps" className="project_link">
+              <p className="proj_head">Expense-Tracker</p>
+              <p className="proj_desc">
+                React.js, react-chart-js2, Node.js, Express.js, Mongodb
+              </p>
+            </a>
+          </div>
+          <br />
+          <div className="inner_proj_div">
+            <a href="https://github.com/Honeyaps" className="project_link">
+              <p className="proj_head">Blog-app</p>
+              <p className="proj_desc">
+                React.js, Node.js, Express.js, Mongodb, Firebase
+              </p>
+            </a>
+          </div>
+          <br />
+          <div className="inner_proj_div">
+            <a href="https://github.com/Honeyaps" className="project_link">
+              <p className="proj_head">E-Commerce</p>
+              <p className="proj_desc">
+                React.js , Node.js, Express.js, Mongodb, Firebase
+              </p>
+            </a>
+          </div>
+          <br />
+          <div className="inner_proj_div">
+            <a href="https://github.com/Honeyaps" className="project_link">
+              <p className="proj_head">Game-Space</p>
+              <p className="proj_desc">HTML, CSS, Bootstrap</p>
+            </a>
+          </div>
         </div>
       </div>
 
-      <br/><br/>
+      <br />
+      <br />
+      <br />
+      {/* HIRE ME */}
+      <div className="hire_sec">
+        <div>
+          <h2>Want to work with me ?</h2>
+          <br />
+          <p>Always feel free to Contact & Hire me</p>
+        </div>
+
+        <div className="insta_btn_div">
+          <a href="https://instagram.com" className="insta_btn">
+            <FiInstagram /> Follow me on Instagram
+          </a>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+
+      {/* Client thinking about me */}
+      <h1 className="wik">Testimonial</h1>
+      <div className="client_contnr">
+        <div className="client_div">
+          <p>
+            "Exceptional MERN Stack developer! Delivered high-quality, scalable
+            applications on time. Great communication and problem-solving
+            skills. Highly recommended for any project."
+          </p>
+          <br />
+          <h3>John</h3>
+        </div>
+
+        <div className="client_div">
+          <p>
+            "Outstanding MERN Stack developer! Consistently delivered top-notch,
+            scalable applications ahead of schedule. Excellent problem-solving
+            abilities and communication skills. Highly reliable and a pleasure
+            to work with."
+          </p>
+          <br />
+          <h3>Cheema</h3>
+        </div>
+      </div>
+
+      <br />
+      <br />
+
+      {/* Contact me */}
+      <div className="contact_contnr">
+        <h1 className="wik">Contact Me</h1>
+        <br /><br />
+        <form onSubmit={handleSubmit}>
+          <div className="two_inputs">
+            <div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                className="inputs1"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+              {errors.name && <p className="error">{errors.name}</p>}
+            </div>
+            <div>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                className="inputs2"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+              {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+          </div>
+          <div className="full_input">
+            <input
+              type="text"
+              name="message"
+              placeholder="Write Something here"
+              className="inputs3"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+            {errors.message && <p className="error">{errors.message}</p>}
+          </div>
+          <button type="submit" className="snd_msg">Send Message</button>
+        </form>
+      </div>
     </>
   );
 }
